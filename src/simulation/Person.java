723654143d97bf9efd112e9820d564cb034d11b2
Urs Gerber <ug.gerber@gmail.com>
2011-10-11 18:22:20 +0200

    modified:   bin/simulation/Person.class 	modified:   results.txt 	modified:   src/simulation/Person.java 	bin/simulation/AncestorChartSimulation.class 	src/simulation/AncestorChartSimulation.java

--- src/simulation/Person.java
+++ src/simulation/Person.java
@@ -1,20 +1,11 @@
 package simulation;
 
-import java.util.ArrayList;
-
 public class Person 
 {
 	public int descendants;
-	//private ArrayList<Person> children;
 	
 	public Person()
 	{
-		//this.children = new ArrayList<Person>();
 		this.descendants = 0;
 	}
-	
-	//public void addChild(Person child)
-	//{
-		//this.children.add(child);
-	//}
 }
